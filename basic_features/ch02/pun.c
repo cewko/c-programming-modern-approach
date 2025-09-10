63e4040090e56c664cb81b78c94c3e54a621d895
cewko <221901489+cewko@users.noreply.github.com>
2025-09-10 16:13:17 +0200

    print a bad pun

---
+++ basic_features/ch02/pun.c
@@ -0,0 +1,14 @@
+/* 
+Name: pun.c
+Purpose: print a bad pun.
+Author: K. N. King
+*/
+
+#include <stdio.h>
+
+int main(void)  /* Beginning of main program */
+{
+    // C99 kind of a comment
+    printf("To C, or not to C: that is the question.\n");
+    return 0;
+}
