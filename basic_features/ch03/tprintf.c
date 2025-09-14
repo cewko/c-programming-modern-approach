46df6384e0b9c9379d08db4dc6bccd0dd8b8150b
cewko <221901489+cewko@users.noreply.github.com>
2025-09-14 13:59:47 +0200

    print int and float values in various formats

---
+++ basic_features/ch03/tprintf.c
@@ -0,0 +1,21 @@
+/* print int and float values in various formats */
+
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    float x;
+
+    i = 40;
+    x = 839.21f;
+
+    printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
+    printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
+
+    return 0;
+}
+
+/*
+    |40|   40|40   |  040|
+    |   839.210| 8.392e+02|839.21    |
+*/
