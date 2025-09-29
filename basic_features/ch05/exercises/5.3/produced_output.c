6115c09df36f2a314b98c4e43ec7ddd2feb4f237
cewko <221901489+cewko@users.noreply.github.com>
2025-09-29 13:44:29 +0200

    solutions to the exercises for 5.3

---
+++ basic_features/ch05/exercises/5.3/produced_output.c
@@ -0,0 +1,6 @@
+// the output is "onetwo".
+// there is no break statement so the execution 
+// falls through to the next case.
+
+// without break, execution continues through cases
+// regardless of whether they match.
