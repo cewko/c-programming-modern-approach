f29779f35cacc9de1ca3dbab80ab3c82ec1e2c25
cewko <221901489+cewko@users.noreply.github.com>
2025-10-23 18:05:09 +0200

    determine message length (shortened version)

---
+++ basic_features/ch07/length2.c
@@ -0,0 +1,14 @@
+/* determine the length of a message (shortened version)*/
+
+#include <stdio.h>
+
+int main(void) {
+    int len = 0;
+
+    printf("Enter a message: ");
+    while (getchar() != '\n')
+        len++;
+    printf("Your message was %d character(s) long.\n", len);
+
+    return 0;
+}
