6c8075e1879da51eb027c839647edf495221878d
cewko <221901489+cewko@users.noreply.github.com>
2025-10-27 17:33:49 +0100

    solutions to the exercises for 7.5

---
+++ basic_features/ch07/exercises/7.5/ex15_create_types.c
@@ -0,0 +1,13 @@
+// C89
+
+typedef signed char Int8;
+typedef short Int16;
+typedef int Int32;
+
+// or use the header (C99+)
+
+#include <stdint.h>
+
+typedef int8_t Int8;
+typedef int16_t Int16;
+typedef int32_t Int32;
