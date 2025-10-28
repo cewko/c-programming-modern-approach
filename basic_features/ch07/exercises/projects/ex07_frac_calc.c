ccacfba8454d7d68bac38e153ae13d4ba67b60b4
cewko <221901489+cewko@users.noreply.github.com>
2025-10-28 18:37:35 +0100

    perform operations on two fractions

---
+++ basic_features/ch07/exercises/projects/ex07_frac_calc.c
@@ -0,0 +1,37 @@
+/* perform operations on two fractions */
+
+#include <stdio.h>
+
+int main(void) {
+    int num1, denom1, num2, denom2, result_num, result_denom;
+    char operator;
+
+    printf("Enter two fractions separated by an operation (+, -, *, /): ");
+    scanf("%d/%d %c %d/%d", &num1, &denom1, &operator, &num2, &denom2);
+
+    switch (operator) {
+        case '+':
+            result_num = num1 * denom2 + num2 * denom1;
+            result_denom = denom1 * denom2;
+            break;
+        case '-':
+            result_num = num1 * denom2 - num2 * denom1;
+            result_denom = denom1 * denom2;
+            break;
+        case '*':
+            result_num = num1 * num2;
+            result_denom = denom1 * denom2;
+            break;
+        case '/':
+            result_num = num1 * denom2;
+            result_denom = denom1 * num2;
+            break;
+        default:
+            printf("Invalid operator\n");
+            return 1;
+    }
+
+    printf("Result: %d/%d\n", result_num, result_denom);
+
+    return 0;
+}
